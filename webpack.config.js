0b2b093405a7640ddd7d25860a2665543c6469f1
gman.park <im@gman.io>
2016-10-09 07:27:57 +0900

    [TASK] config modified

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+var path = require('path');
+var webpack = require('webpack');
+// var webpackConfigModuleLoader = require('./webpack.config.moduleLoader');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var webpackOutputPublicPath = '/dist/';
+
+module.exports = {
+    // devtool: 'eval',
+    debug: false,
+    entry: [
+        // 'react-hot-loader/patch',
+        // 'webpack-dev-server/client?http://localhost:3000',
+        // 'webpack/hot/only-dev-server',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: webpackOutputPublicPath
+    },
+    plugins: [
+        new ExtractTextPlugin("[name].css"),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                // This has effect on the react lib size
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['babel'],
+                include: path.join(__dirname, 'src')
+            },
+            {
+                test: /\.css$/, loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.sass/,
+                loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+            },
+            {
+                test: /\.scss/,
+                loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+            },
+            {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
+            },
+            {
+                test: /\.styl/,
+                loader: 'style-loader!css-loader!stylus-loader'
+            },
+            {
+                test: /\.(png|jpg|gif|woff|woff2)$/,
+                loader: 'url-loader?limit=8192'
+            },
+            {
+                test: /\.(mp4|ogg|svg)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+
+    devServer: {
+        port: 3000,
+        publicPath: webpackOutputPublicPath,
+        hot: true,
+        inline: true,
+        colors: true,
+        historyApiFallback: true,
+        compress: true,
+        quiet: false,
+        progress: true
+    }
+}
